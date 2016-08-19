ab2650c942cdeeac816f9a0efeafe8f93903c160
Yusuf Kelo <yusufkelo@Yusufs-MacBook-Pro.local>
2016-08-19 09:00:32 +0400

    init

--- config/oAuth.js
+++ config/oAuth.js
@@ -2,7 +2,7 @@ module.exports={
     facebookAuth:{
         clientId:'537652703088804',
         clientSecret:'47313c3f20ef3b647dc3ab73b984f5c8',
-        callBackUrl:'http://localhost:3000/auth/facebook/callback',
+        callBackUrl:'http://cookingchallenge.azurewebsites.net/auth/facebook/callback',//'http://localhost:3000/auth/facebook/callback',
          profileFields: ['id', 'displayName', 'photos', 'email']
     }
 }
