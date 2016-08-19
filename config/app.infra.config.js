adcf93df89769b6559c73b30faefd8da641a6881
Yusuf Kelo <yusufkelo@Yusufs-MacBook-Pro.local>
2016-08-19 08:26:27 +0400

    init

--- config/app.infra.config.js
+++ config/app.infra.config.js
@@ -1,8 +1,7 @@
 var config={
-    data_source:'mongodb://localhost/teamsync',
+    data_source:'mongodb://bookingdb_admin:aaabbbccc111222333@ds038739.mlab.com:38739/flightbookingmlab',//'mongodb://ykel:engine56@ds035448.mlab.com:35448/mydb',//'mongodb://localhost/teamsync',
     app_secret:'xxx_x727987#@**'
 }
 
 
 module.exports=config;
-
